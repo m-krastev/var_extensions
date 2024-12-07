b8fee99766e79ce239b75b27d6554b213fa20bae
lisannbecker <lisann.becker@gmx.de>
2024-12-07 15:30:26 +0100

    reconstruction of week1 maze

---
+++ maze_pipeline/notebooks/session_1/vsfm_matches_full/reconstruct_dense_full.nvm.cmvs/00/pmvs.bat
@@ -0,0 +1,9 @@
+pmvs2 pmvs/ option-0000
+pmvs2 pmvs/ option-0001
+pmvs2 pmvs/ option-0002
+pmvs2 pmvs/ option-0003
+pmvs2 pmvs/ option-0004
+pmvs2 pmvs/ option-0005
+pmvs2 pmvs/ option-0006
+pmvs2 pmvs/ option-0007
+
